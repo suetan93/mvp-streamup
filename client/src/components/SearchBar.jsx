8e83aa3c80082d761b279eabfe5627ab219d1386
Susan Tan <suetan93@gmail.com>
2020-09-15 11:31:44 -0700

    empty search disabled

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -5,8 +5,12 @@ const SearchBar = (props) => {
 
   const handleSearch = (e) => {
     e.preventDefault();
-    props.changeHome();
-    props.getSearchResults(search.toLowerCase())
+    if (!search) {
+      alert('Search field empty')
+    } else {
+      props.changeHome();
+      props.getSearchResults(search.toLowerCase())
+    }
   }
 
   return (
@@ -14,7 +18,14 @@ const SearchBar = (props) => {
       <form onSubmit={handleSearch}>
       <input className="searchBox" type="text" placeholder="Search movie or show" value={search} onChange={e => onSearch(e.target.value)} />
       &nbsp;
-      <input className='bttn' type="submit" value="Search" />
+      <button className="searchBttn">
+        <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false">
+          <g fill="none">
+            <path d="m13 24c6.0751322 0 11-4.9248678 11-11 0-6.07513225-4.9248678-11-11-11-6.07513225 0-11 4.92486775-11 11 0 6.0751322 4.92486775 11 11 11zm8-3 9 9">
+            </path>
+            </g>
+        </svg>
+      </button>
       </form>
     </div>
   )
